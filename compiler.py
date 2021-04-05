ab1f5b3ada934af650a549ffc20405b1478ab2c5
vsaeide <vsaeide7798@gmail.com>
2021-04-05 21:03:20 +0430

    error handling

--- compiler.py
+++ compiler.py
@@ -5,15 +5,17 @@ class Scanner:
     keyword = ['if', 'else', 'void', 'int', 'while', 'break', 'switch', 'default', 'case', 'return', 'for']
     symbol = [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-','<', '=', '*']
     whitespace = [' ', '\n', '\r', '\t', '\v', '\f']
-    letter = ['a', 'A', 'b', 'B', 'C', 'c', 'd', 'D', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I' 'i', 'J', 'j', 'K',
-              'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u',
-              'v', 'V', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z']
+    # letter = ['a', 'A', 'b', 'B', 'C', 'c', 'd', 'D', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I' 'i', 'J', 'j', 'K',
+    #           'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u',
+    #           'v', 'V', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z']
     #token_types = ['digit', 'letter', 'ID', 'Keyword', 'Symbol', 'Whitespace']
     #states = ['start','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','num', 'ide', 'alphabet', 'other', 'comment', 'symb']
 
-    final_states=['2','4','5','7','9','c','f']
-    counter = 0
-    input = 0
+    error_types=['Invalid input','Unmatched comment' ,'Unclosed comment']
+    symbol_table=[]
+    # final_states=['2','4','5','7','9','c','f']
+    # counter = 0
+    # input = 0
     array = ""
     state ='0'
 
@@ -21,46 +23,38 @@ class Scanner:
 
         self.line_num = 1
         self.token = []
-        #self.lex_error = []
+        self.lex_error = []
         self.program = open(input, 'r').read()
-        # print(self.program)
-        # print(self.program[0:5])
 
         self.start_loc=0
         self.loc =0
         self.file_cap = len(self.program)
 
-        # self.lexical_errors = open('lexical_errors.txt', 'r').read()
-        # self.symbol_table = open('lexical_errors.txt', 'r').read()
-        # self.token_file = open('tokens.txt', 'r').read()
+    def check_NUM(self,num):
 
-
+        for digit in num:
+            if not self.is_digit(digit):
+                return False
+        return True
 
     def get_next_token(self):
 
         temp_arr=""
 
-
         while(self.loc<self.file_cap):
-            #print(self.loc, self.program[self.loc])
 
             if self.state=='0':
                 self.start_loc=self.loc
 
-            #self.next_char()
             self.dfa_navigation(self.program[self.loc])
 
             token=self.program[self.start_loc:self.loc]
-            #print("len is ", len(token))
-            #print(token)
-            #print(self.start_loc,self.loc)
 
             if self.program[self.loc]=='\n':
                 if len(temp_arr):
                     self.array += str(self.line_num) + ". " + temp_arr+"\n"
                     temp_arr=""
                 self.line_num+=1
-                #print("next line")
 
             if self.state=='2':
 
@@ -68,10 +62,17 @@ class Scanner:
                     temp_arr+="( KEYWORD,"+token+" )"
                 else:
                     temp_arr +="( ID,"+ token+ " )"
+
+                if token not in self.symbol_table:
+                    self.symbol_table.append(token)
                 self.state='0'
 
             elif self.state=='4':
-                temp_arr +="( NUM, "+token+" )"
+
+                if self.check_NUM(token):
+                    temp_arr +="( NUM, "+token+" )"
+                else:
+                    self.lex_error.append([self.line_num,token,"Invalid number"])
                 self.state='0'
 
             elif self.state == '5' or self.state=='7' or self.state == '9':
@@ -89,6 +90,10 @@ class Scanner:
                 self.state='0'
                 self.loc+=1
                 #print("( WHITESPACE, ", token," )")
+            elif self.state in self.error_types:
+                self.lex_error.append([self.line_num, self.program[self.start_loc:self.loc+1], self.state])
+                self.state='0'
+                self.loc += 1
             else:
                 self.loc+=1
 
@@ -97,6 +102,8 @@ class Scanner:
         f = open("tokens.txt", "w")
         f.write(self.array)
         f.close()
+        print(self.symbol_table)
+        print(self.lex_error)
 
 
     def lookahead_chr(self):
@@ -114,6 +121,7 @@ class Scanner:
     #         return chr
 
     #####        return types
+
     def is_symbol(self, chr):
         if chr in self.symbol:
             return True
@@ -136,16 +144,15 @@ class Scanner:
 
     ######
 
-    def do_whitespace(self, whitespace):
-        if whitespace == '\n':
-            self.line_num += 1
+    # def do_whitespace(self, whitespace):
+    #     if whitespace == '\n':
+    #         self.line_num += 1
 
-    def Ignore_whitespace(self):  # for skipping white spaces
-        while self.loc in self.whitespace:
-            self.next_char()
+    # def Ignore_whitespace(self):  # for skipping white spaces
+    #     while self.loc in self.whitespace:
+    #         self.next_char()
 
     def dfa_navigation(self, chr):
-        #self.counter += 1
 
         if self.state == '0':  # write if for every state after start, based on DFA
             if self.is_digit(chr):
@@ -163,11 +170,16 @@ class Scanner:
                     self.state='5'
             elif self.is_whitespace(chr):
                 self.state='f'
+            else:
+                self.state='Invalid input'
 
         elif self.state=='1':
 
             if not (self.is_digit(chr) or self.is_letter(chr)):
-                self.state='2'
+                if chr=="!":
+                    self.state='Invalid input'
+                else:
+                    self.state='2'
             # else state 1
         # elif self.state=='2':
         #     accept
@@ -188,6 +200,8 @@ class Scanner:
         elif self.state=='8':
             if not chr=='/':
                 self.state='9'
+            else:
+                self.state="Unmatched comment"
         # elif self.state=='9':
         #     accept
         elif self.state=='a':
@@ -203,6 +217,8 @@ class Scanner:
         elif self.state=='d':
             if chr=="*":
                 self.state='e'
+            elif self.loc==self.file_cap-1:
+                self.state='Unclosed comment'
         elif self.state=='e':
             if chr=="/":
                 self.state='c'
